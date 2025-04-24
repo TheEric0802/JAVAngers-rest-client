76ad24c38768406767ec82fb9a5b7a4f85f99437
Eric Schmidt <eric0802@web.de>
2025-04-24 09:44:23 +0200

    add class RickMortyCharacter

---
+++ src/main/java/org/example/javangersrestclient/model/RickMortyCharacter.java
@@ -0,0 +1,11 @@
+package org.example.javangersrestclient.model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class RickMortyCharacter {
+
+    private int id;
+    private String name;
+    private String species;
+}
