674ae40a1c19429bd65467ea0ac4f4f280ff16de
Eric Schmidt <eric0802@web.de>
2025-04-24 10:11:52 +0200

    add class RickMortyAllCharactersInfoDTO

---
+++ src/main/java/org/example/javangersrestclient/model/dto/RickMortyAllCharactersInfoDTO.java
@@ -0,0 +1,10 @@
+package org.example.javangersrestclient.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class RickMortyAllCharactersInfoDTO {
+
+    private int pages;
+}
