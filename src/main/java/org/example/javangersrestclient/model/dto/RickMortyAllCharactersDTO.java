b101df7c457e0617bb8c9c5e405ec3dc81ecd65c
Eric Schmidt <eric0802@web.de>
2025-04-24 09:49:53 +0200

    add class RickMortyAllCharactersDTO

---
+++ src/main/java/org/example/javangersrestclient/model/dto/RickMortyAllCharactersDTO.java
@@ -0,0 +1,10 @@
+package org.example.javangersrestclient.model.dto;
+
+import org.example.javangersrestclient.model.RickMortyCharacter;
+
+import java.util.List;
+
+public class RickMortyAllCharactersDTO {
+
+    private List<RickMortyCharacter> results;
+}
