244e3f347871b5240280c2d620ab9a830e8fad8a
Eric Schmidt <eric0802@web.de>
2025-04-24 11:10:12 +0200

    add class UserController

---
+++ src/main/java/org/example/javangersrestclient/controller/UserController.java
@@ -0,0 +1,9 @@
+package org.example.javangersrestclient.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @RequestMapping("/api/species-statistic") @RequiredArgsConstructor
+public class UserController {
+}
