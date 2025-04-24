1897c8ee8230c5ecac7db3f8799c0c74a7066e53
Eric Schmidt <eric0802@web.de>
2025-04-24 11:13:05 +0200

    add class UserService

--- src/main/java/org/example/javangersrestclient/controller/UserController.java
+++ src/main/java/org/example/javangersrestclient/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.example.javangersrestclient.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.javangersrestclient.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
