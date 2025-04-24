f76cabe4eecd88e3f41f5aed77ab90194d577b87
Eric Schmidt <eric0802@web.de>
2025-04-24 11:24:46 +0200

    add getAllUsers

--- src/main/java/org/example/javangersrestclient/controller/UserController.java
+++ src/main/java/org/example/javangersrestclient/controller/UserController.java
@@ -1,12 +1,21 @@
 package org.example.javangersrestclient.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.javangersrestclient.model.ReqresUser;
 import org.example.javangersrestclient.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController @RequestMapping("/api/users") @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public List<ReqresUser> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
--- src/main/java/org/example/javangersrestclient/service/UserService.java
+++ src/main/java/org/example/javangersrestclient/service/UserService.java
@@ -1,7 +1,11 @@
 package org.example.javangersrestclient.service;
 
+import org.example.javangersrestclient.model.ReqresUser;
+import org.example.javangersrestclient.model.dto.ReqresAllUsersDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -10,5 +14,21 @@ public class UserService {
 
     public UserService(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder.baseUrl("https://reqres.in/api/users").build();
+    }
+
+    public List<ReqresUser> getAllUsers() {
+        int currentPage = 1;
+        int totalPages = 0;
+        List<ReqresUser> result = new java.util.ArrayList<>();
+
+        do {
+            ReqresAllUsersDTO page = restClient.get().uri("?page=" + currentPage).retrieve().body(ReqresAllUsersDTO.class);
+            assert page != null;
+            totalPages = page.getTotal_pages();
+            currentPage++;
+            result.addAll(page.getData());
+        } while (currentPage <= totalPages);
+
+        return result;
     }
 }
