dc542b68376116bbf139e11a8538768ddf4fc6f0
Eric Schmidt <eric0802@web.de>
2025-04-24 09:40:59 +0200

    Initial commit

---
+++ src/main/java/org/example/javangersrestclient/JavAngersRestClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.javangersrestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAngersRestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavAngersRestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javangersrestclient/JavAngersRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javangersrestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAngersRestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
